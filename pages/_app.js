4c9113cef4d8cbeef20e7a3f3edee7d37f97523d
Lhomme Aymeric <37447949+HeinTouchableFR@users.noreply.github.com>
2021-01-31 16:58:41 +0100

    fix: ajout indicateur chargement

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,16 @@ import '../style/globals.css'
 import 'semantic-ui-css/semantic.min.css'
 import 'rsuite/dist/styles/rsuite-default.css';
 
-function MyApp({ Component, pageProps }) {
-  return <><Component {...pageProps} /></>
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+//Binding events.
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+function MyApp({Component, pageProps}) {
+    return <><Component {...pageProps} /></>
 }
 
 export default MyApp
