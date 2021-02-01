e8c2e8ac8e1cf40e35d6229beec29dc87cfbe5cb
Lhomme Aymeric <37447949+HeinTouchableFR@users.noreply.github.com>
2021-02-01 22:04:22 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
         MONGO_URI: "mongodb+srv://dev:dev@dev.0flva.mongodb.net/cmsjs",
-        //URL: 'https://cmsjs.aymericlhomme.fr'
-        URL: 'http://localhost:3000'
+        URL: 'https://cmsjs.aymericlhomme.fr'
+        //URL: 'http://localhost:3000'
     }
 }
