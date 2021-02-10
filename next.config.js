17643055e83c4f7d9b098e268a7f5966da1f02bc
Lhomme Aymeric <37447949+HeinTouchableFR@users.noreply.github.com>
2021-02-10 22:08:03 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,9 @@ module.exports = {
     MONGO_URI: process.env.MONGO_URI,
     URL: process.env.SERVER,
     GCLOUD_PROJECT_ID: process.env.GCLOUD_PROJECT_ID,
-    GCLOUD_APPLICATION_CREDENTIALS: "pages/api/services/cmsjs-fa640-firebase-adminsdk-nubd4-7fa4af8ee5.json",
     GCLOUD_STORAGE_BUCKET_URL: process.env.GCLOUD_STORAGE_BUCKET_URL,
     CLIENT_EMAIL: process.env.CLIENT_EMAIL,
-    PRIVATE_KEY: process.env.PRIVATE_KEY,
+    PRIVATE_KEY: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
   },
   images: {
     domains: ['firebasestorage.googleapis.com'],
