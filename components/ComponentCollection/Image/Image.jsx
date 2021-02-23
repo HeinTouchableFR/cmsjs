ecdc31b503f78be3b49b3ce0435f8c467411289d
Lhomme Aymeric <37447949+HeinTouchableFR@users.noreply.github.com>
2021-02-23 20:29:16 +0100

    Update Image.jsx

--- components/ComponentCollection/Image/Image.jsx
+++ components/ComponentCollection/Image/Image.jsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
 //Style
 import styles from './Image.module.scss';
-//Utils
-import useTranslation from 'intl/UseTranslation';
 //Components
 import FileManager from 'components/FileManager/FileManager';
 
 export default function Image({ element, onElementValeurChange }) {
-    const { t } = useTranslation();
 
     const [currentFiles, setCurrentFiles] = useState([]);
 
