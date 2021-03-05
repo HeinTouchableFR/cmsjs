d3f59113f5d867183454d6008d64bd81d6b19c78
Lhomme Aymeric <37447949+HeinTouchableFR@users.noreply.github.com>
2021-03-05 23:44:18 +0100

    fix: ajout de fonts

--- components/ComponentCollection/Title/Title.jsx
+++ components/ComponentCollection/Title/Title.jsx
@@ -128,17 +128,182 @@ export default function Title({ element, onElementValeurChange }) {
 
     const fontsOptions = [
         {
-            key: 'Roboto',
-            text: 'Roboto',
-            value: 'Roboto',
+            key: 'Abril Fatface',
+            text: 'Abril Fatface',
+            value: 'Abril Fatface',
             content: (
                 <span
                     css={css`
-                        font-family: Roboto;
+                        font-family: Abril Fatface;
                         font-weight: normal;
                     `}
                 >
-                    Roboto
+                    Abril Fatface
+                </span>
+            ),
+        },
+        {
+            key: 'Alegreya',
+            text: 'Alegreya',
+            value: 'Alegreya',
+            content: (
+                <span
+                    css={css`
+                        font-family: Alegreya;
+                        font-weight: normal;
+                    `}
+                >
+                    Alegreya
+                </span>
+            ),
+        },
+        {
+            key: 'Archivo',
+            text: 'Archivo',
+            value: 'Archivo',
+            content: (
+                <span
+                    css={css`
+                        font-family: Archivo;
+                        font-weight: normal;
+                    `}
+                >
+                    Archivo
+                </span>
+            ),
+        },
+        {
+            key: 'B612',
+            text: 'B612',
+            value: 'B612',
+            content: (
+                <span
+                    css={css`
+                        font-family: B612;
+                        font-weight: normal;
+                    `}
+                >
+                    B612
+                </span>
+            ),
+        },
+        {
+            key: 'BioRhyme',
+            text: 'BioRhyme',
+            value: 'BioRhyme',
+            content: (
+                <span
+                    css={css`
+                        font-family: BioRhyme;
+                        font-weight: normal;
+                    `}
+                >
+                    BioRhyme
+                </span>
+            ),
+        },
+        {
+            key: 'Alegreya',
+            text: 'Alegreya',
+            value: 'Alegreya',
+            content: (
+                <span
+                    css={css`
+                        font-family: Alegreya;
+                        font-weight: normal;
+                    `}
+                >
+                    Alegreya
+                </span>
+            ),
+        },
+        {
+            key: 'Cairo',
+            text: 'Cairo',
+            value: 'Cairo',
+            content: (
+                <span
+                    css={css`
+                        font-family: Cairo;
+                        font-weight: normal;
+                    `}
+                >
+                    Cairo
+                </span>
+            ),
+        },
+        {
+            key: 'Coiny',
+            text: 'Coiny',
+            value: 'Coiny',
+            content: (
+                <span
+                    css={css`
+                        font-family: Coiny;
+                        font-weight: normal;
+                    `}
+                >
+                    Coiny
+                </span>
+            ),
+        },
+        {
+            key: 'Cormorant',
+            text: 'Cormorant',
+            value: 'Cormorant',
+            content: (
+                <span
+                    css={css`
+                        font-family: Cormorant;
+                        font-weight: normal;
+                    `}
+                >
+                    Cormorant
+                </span>
+            ),
+        },
+        {
+            key: 'Crimson Text',
+            text: 'Crimson Text',
+            value: 'Crimson Text',
+            content: (
+                <span
+                    css={css`
+                        font-family: Crimson Text;
+                        font-weight: normal;
+                    `}
+                >
+                    Crimson Text
+                </span>
+            ),
+        },
+        {
+            key: 'Frank Ruhl Libre',
+            text: 'Frank Ruhl Libre',
+            value: 'Frank Ruhl Libre',
+            content: (
+                <span
+                    css={css`
+                        font-family: Frank Ruhl Libre;
+                        font-weight: normal;
+                    `}
+                >
+                    Frank Ruhl Libre
+                </span>
+            ),
+        },
+        {
+            key: 'IBM Plex Serif',
+            text: 'IBM Plex Serif',
+            value: 'IBM Plex Serif',
+            content: (
+                <span
+                    css={css`
+                        font-family: IBM Plex Serif;
+                        font-weight: normal;
+                    `}
+                >
+                    IBM Plex Serif
                 </span>
             ),
         },
@@ -154,6 +319,201 @@ export default function Title({ element, onElementValeurChange }) {
                     `}
                 >
                     Impact
+                </span>
+            ),
+        },
+        {
+            key: 'Karla',
+            text: 'Karla',
+            value: 'Karla',
+            content: (
+                <span
+                    css={css`
+                        font-family: Karla;
+                        font-weight: normal;
+                    `}
+                >
+                    Karla
+                </span>
+            ),
+        },
+        {
+            key: 'Lora',
+            text: 'Lora',
+            value: 'Lora',
+            content: (
+                <span
+                    css={css`
+                        font-family: Lora;
+                        font-weight: normal;
+                    `}
+                >
+                    Lora
+                </span>
+            ),
+        },
+        {
+            key: 'Montserrat',
+            text: 'Montserrat',
+            value: 'Montserrat',
+            content: (
+                <span
+                    css={css`
+                        font-family: Montserrat;
+                        font-weight: normal;
+                    `}
+                >
+                    Montserrat
+                </span>
+            ),
+        },
+        {
+            key: 'Muli',
+            text: 'Muli',
+            value: 'Muli',
+            content: (
+                <span
+                    css={css`
+                        font-family: Muli;
+                        font-weight: normal;
+                    `}
+                >
+                    Muli
+                </span>
+            ),
+        },
+        {
+            key: 'Playfair Display',
+            text: 'Playfair Display',
+            value: 'Playfair Display',
+            content: (
+                <span
+                    css={css`
+                        font-family: Playfair Display;
+                        font-weight: normal;
+                    `}
+                >
+                    Playfair Display
+                </span>
+            ),
+        },
+        {
+            key: 'PT Banana Split',
+            text: 'PT Banana Split',
+            value: 'PT Banana Split',
+            content: (
+                <span
+                    css={css`
+                        font-family: PT Banana Split;
+                        font-weight: normal;
+                    `}
+                >
+                    PT Banana Split
+                </span>
+            ),
+        },
+        {
+            key: 'Roboto',
+            text: 'Roboto',
+            value: 'Roboto',
+            content: (
+                <span
+                    css={css`
+                        font-family: Roboto;
+                        font-weight: normal;
+                    `}
+                >
+                    Roboto
+                </span>
+            ),
+        },
+        {
+            key: 'Spectral',
+            text: 'Spectral',
+            value: 'Spectral',
+            content: (
+                <span
+                    css={css`
+                        font-family: Spectral;
+                        font-weight: normal;
+                    `}
+                >
+                    Spectral
+                </span>
+            ),
+        },
+        {
+            key: 'Titillium Web',
+            text: 'Titillium Web',
+            value: 'Titillium Web',
+            content: (
+                <span
+                    css={css`
+                        font-family: Titillium Web;
+                        font-weight: normal;
+                    `}
+                >
+                    Titillium Web
+                </span>
+            ),
+        },
+        {
+            key: 'TrashHand',
+            text: 'TrashHand',
+            value: 'TrashHand',
+            content: (
+                <span
+                    css={css`
+                        font-family: TrashHand;
+                        font-weight: normal;
+                    `}
+                >
+                    TrashHand
+                </span>
+            ),
+        },
+        {
+            key: 'Ubuntu',
+            text: 'Ubuntu',
+            value: 'Ubuntu',
+            content: (
+                <span
+                    css={css`
+                        font-family: Ubuntu;
+                        font-weight: normal;
+                    `}
+                >
+                    Ubuntu
+                </span>
+            ),
+        },
+        {
+            key: 'Varela',
+            text: 'Varela',
+            value: 'Varela',
+            content: (
+                <span
+                    css={css`
+                        font-family: Varela;
+                        font-weight: normal;
+                    `}
+                >
+                    Varela
+                </span>
+            ),
+        },
+        {
+            key: 'Vollkorn',
+            text: 'Vollkorn',
+            value: 'Vollkorn',
+            content: (
+                <span
+                    css={css`
+                        font-family: Vollkorn;
+                        font-weight: normal;
+                    `}
+                >
+                    Vollkorn
                 </span>
             ),
         },
