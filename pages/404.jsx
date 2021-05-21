16dee3957336e7e8e09dcca8c44d411e0b709101
Lhomme Aymeric <37447949+HeinTouchableFR@users.noreply.github.com>
2021-05-21 17:43:50 +0200

    1ere version page erreur 404

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,6 +1,7 @@
 import Header from 'container/Sites/Header/Header';
 import React, { useState } from 'react';
 import { useSettings } from 'context/settings';
+import Link from 'next/link';
 import Footer from '../container/Sites/Footer/Footer';
 
 export default function Error404() {
@@ -14,17 +15,75 @@ export default function Error404() {
 
     return (
         <>
-            <Header
-                settings={settings}
-                setShowRender={setShowRender}
-                showRender={showRender}
-                post={post}
-            />
-            <div className='container'>:(</div>
-            <Footer
-                showRender={showRender}
-                setShowRender={setShowRender}
-            />
+            <section className='wrapper'>
+                <div className='container'>
+                    <div
+                        id='scene'
+                        className='scene'
+                        data-hover-only='false'
+                    >
+                        <div
+                            className='circle'
+                            data-depth='1.2'
+                        />
+                        <div
+                            className='one'
+                            data-depth='0.9'
+                        >
+                            <div className='content'>
+                                <span className='piece' />
+                                <span className='piece' />
+                                <span className='piece' />
+                            </div>
+                        </div>
+                        <div
+                            className='two'
+                            data-depth='0.60'
+                        >
+                            <div className='content'>
+                                <span className='piece' />
+                                <span className='piece' />
+                                <span className='piece' />
+                            </div>
+                        </div>
+                        <div
+                            className='three'
+                            data-depth='0.40'
+                        >
+                            <div className='content'>
+                                <span className='piece' />
+                                <span className='piece' />
+                                <span className='piece' />
+                            </div>
+                        </div>
+                        <p
+                            className='p404'
+                            data-depth='0.50'
+                        >
+                            404
+                        </p>
+                        <p
+                            className='p404'
+                            data-depth='0.10'
+                        >
+                            404
+                        </p>
+                    </div>
+                    <div className='text'>
+                        <article>
+                            <p>
+                                Uh oh! Looks like you got lost.
+                                <br />
+                                Go back to the homepage if you dare!
+                            </p>
+                            <Link href='/'>
+                                <a>i dare !</a>
+                            </Link>
+                        </article>
+                    </div>
+
+                </div>
+            </section>
         </>
     );
 }
