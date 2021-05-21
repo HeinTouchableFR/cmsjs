5d304949cd82fd330de98e532af2867f2c8a68db
Lhomme Aymeric <37447949+HeinTouchableFR@users.noreply.github.com>
2021-05-21 14:42:46 +0200

    Ajout des exclusions des pages admin dans le sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,22 @@
 module.exports = {
     siteUrl: process.env.server,
     generateRobotsTxt: true,
-    exclude: ['/server-sitemap.xml', '/admin/attributes'], // <= exclude here
+    exclude: ['/server-sitemap.xml',
+        '/admin/attributes',
+        '/admin/attributes/add',
+        '/admin/categories',
+        '/admin/categories/add',
+        '/admin/dashboard',
+        '/admin/install',
+        '/admin/login',
+        '/admin/templates',
+        '/admin/menus',
+        '/admin/menus/add',
+        '/admin/pages',
+        '/admin/pages/add',
+        '/admin/products',
+        '/admin/products/add',
+    ], // <= exclude here
     robotsTxtOptions: {
         additionalSitemaps: [
             `${process.env.server}/server-sitemap.xml`,
